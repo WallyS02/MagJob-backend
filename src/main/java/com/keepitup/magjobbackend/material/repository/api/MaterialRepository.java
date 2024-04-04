01a448e0a0fa479d453311ddce731742c513681d
WallyS02 <sebkutny@gmail.com>
2024-04-04 13:07:11 +0200

    Material repositories backend

--- src/main/java/com/keepitup/magjobbackend/material/repository/api/MaterialRepository.java
+++ src/main/java/com/keepitup/magjobbackend/material/repository/api/MaterialRepository.java
@@ -1,4 +1,24 @@
 package com.keepitup.magjobbackend.material.repository.api;
 
-public interface MaterialRepository {
+import com.keepitup.magjobbackend.material.entity.Material;
+import com.keepitup.magjobbackend.organization.entity.Organization;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.math.BigInteger;
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Optional;
+
+public interface MaterialRepository extends JpaRepository<Material, BigInteger> {
+    Optional<Material> findByTitle(String title);
+
+    List<Material> findAllByDescription(String description);
+
+    List<Material> findAllBySize(Long size);
+
+    List<Material> findAllByContentType(String contentType);
+
+    List<Material> findAllByDateOfCreation(ZonedDateTime dateOfCreation);
+
+    List<Material> findAllByOrganization(Organization organization);
 }

a9b247958ee1984343206d4bf3cc5c41b22a3cc3
WallyS02 <sebkutny@gmail.com>
2024-04-04 13:20:33 +0200

    Material services backend

--- src/main/java/com/keepitup/magjobbackend/materialreceiver/service/api/MaterialReceiverService.java
+++ src/main/java/com/keepitup/magjobbackend/materialreceiver/service/api/MaterialReceiverService.java
@@ -1,4 +1,27 @@
-package com.keepitup.magjobbackend.material.service.api;
+package com.keepitup.magjobbackend.materialreceiver.service.api;
+
+import com.keepitup.magjobbackend.material.entity.Material;
+import com.keepitup.magjobbackend.materialreceiver.entity.MaterialReceiver;
+import com.keepitup.magjobbackend.member.entity.Member;
+
+import java.math.BigInteger;
+import java.util.List;
+import java.util.Optional;
 
 public interface MaterialReceiverService {
+    Optional<MaterialReceiver> find(BigInteger id);
+
+    List<MaterialReceiver> findAll();
+
+    List<MaterialReceiver> findAllByMember(Member member);
+
+    List<MaterialReceiver> findAllByMaterial(Material material);
+
+    Optional<MaterialReceiver> findByMemberAndMaterial(Member member, Material material);
+
+    void create(MaterialReceiver materialReceiver);
+
+    void delete(BigInteger id);
+
+    void update(MaterialReceiver materialReceiver);
 }

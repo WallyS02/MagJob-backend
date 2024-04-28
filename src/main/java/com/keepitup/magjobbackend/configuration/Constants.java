f1b5842f6965b992b041fce5415bfec3cb165408
kmonieczny <kmonieczny@gmail.com>
2024-04-28 18:58:22 +0200

    Added roles and roleMember creation in createOrganization method, added Constants file

---
+++ src/main/java/com/keepitup/magjobbackend/configuration/Constants.java
@@ -0,0 +1,5 @@
+package com.keepitup.magjobbackend.configuration;
+
+public class Constants {
+    public static final String[] defaultRoleNames = {"Owner", "Moderator", "Member"};
+}
--- src/main/java/com/keepitup/magjobbackend/organization/controller/impl/OrganizationDefaultController.java
+++ src/main/java/com/keepitup/magjobbackend/organization/controller/impl/OrganizationDefaultController.java
@@ -1,5 +1,6 @@
 package com.keepitup.magjobbackend.organization.controller.impl;
 
+import com.keepitup.magjobbackend.configuration.Constants;
 import com.keepitup.magjobbackend.configuration.KeycloakController;
 import com.keepitup.magjobbackend.member.entity.Member;
 import com.keepitup.magjobbackend.member.service.api.MemberService;
@@ -8,6 +9,10 @@ import com.keepitup.magjobbackend.organization.dto.*;
 import com.keepitup.magjobbackend.organization.entity.Organization;
 import com.keepitup.magjobbackend.organization.function.*;
 import com.keepitup.magjobbackend.organization.service.api.OrganizationService;
+import com.keepitup.magjobbackend.role.entity.Role;
+import com.keepitup.magjobbackend.role.service.api.RoleService;
+import com.keepitup.magjobbackend.rolemember.entity.RoleMember;
+import com.keepitup.magjobbackend.rolemember.service.api.RoleMemberService;
 import com.keepitup.magjobbackend.user.entity.User;
 import com.keepitup.magjobbackend.user.service.api.UserService;
 import lombok.extern.java.Log;
@@ -18,6 +23,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigInteger;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -30,6 +36,8 @@ public class OrganizationDefaultController implements OrganizationController {
     private final UpdateOrganizationWithRequestFunction updateOrganizationWithRequest;
     private final MemberService memberService;
     private final UserService userService;
+    private final RoleService roleService;
+    private final RoleMemberService roleMemberService;
     private final KeycloakController keycloakController;
 
     @Autowired
@@ -37,6 +45,8 @@ public class OrganizationDefaultController implements OrganizationController {
             OrganizationService service,
             MemberService memberService,
             UserService userService,
+            RoleService roleService,
+            RoleMemberService roleMemberService,
             OrganizationsToResponseFunction organizationsToResponse,
             OrganizationToResponseFunction organizationToResponse,
             RequestToOrganizationFunction requestToOrganization,
@@ -46,6 +56,8 @@ public class OrganizationDefaultController implements OrganizationController {
         this.service = service;
         this.memberService = memberService;
         this.userService = userService;
+        this.roleService = roleService;
+        this.roleMemberService = roleMemberService;
         this.organizationsToResponse = organizationsToResponse;
         this.organizationToResponse = organizationToResponse;
         this.requestToOrganization = requestToOrganization;
@@ -77,6 +89,7 @@ public class OrganizationDefaultController implements OrganizationController {
 
     @Override
     public GetOrganizationResponse createOrganization(PostOrganizationRequest postOrganizationRequest) {
+        Map<String, String> roleName2groupExternalId;
         Optional<Organization> organization = service.findByName(postOrganizationRequest.getName());
         if (organization.isPresent()) {
             throw new ResponseStatusException(HttpStatus.CONFLICT);
@@ -86,14 +99,33 @@ public class OrganizationDefaultController implements OrganizationController {
             Optional<Organization> createdOrganization = service.findByName(postOrganizationRequest.getName());
 
             if (user.isPresent() && createdOrganization.isPresent()) {
+                roleName2groupExternalId =
+                        keycloakController.createGroupRepresentation(createdOrganization.get().getName(), user.get().getExternalId());
+
+                for (String roleName : Constants.defaultRoleNames) {
+                    roleService.create(Role.builder()
+                            .name(roleName)
+                            .organization(createdOrganization.get())
+                            .externalId(roleName2groupExternalId.get(roleName))
+                            .build());
+                }
+
                 memberService.create(Member.builder()
                         .pseudonym("Owner")
                         .organization(createdOrganization.get())
                         .user(user.get())
                         .build());
-            }
 
-            keycloakController.createGroupRepresentation(createdOrganization.get().getName(), user.get().getExternalId());
+                Optional<Member> ownerMember = memberService.findByUserAndOrganization(user.get(), createdOrganization.get());
+                Optional<Role> ownerRole = roleService.findByExternalId(roleName2groupExternalId.get("Owner"));
+
+                if (ownerMember.isPresent() && ownerRole.isPresent()) {
+                    roleMemberService.create(RoleMember.builder()
+                            .role(ownerRole.get())
+                            .member(ownerMember.get())
+                            .build());
+                }
+            }
         }
         return service.findByName(postOrganizationRequest.getName())
                 .map(organizationToResponse)
